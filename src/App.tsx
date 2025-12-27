33c4089fc7ecd1f0be7d6c0d49c87570fcd05f51
Jenny <jennyle.dev@gmail.com>
2025-12-26 21:31:26 -0700

    Add click-outside dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,9 @@ import ClickOutsideDropdown from './patterns/ClickOutsideDropdown';
 const App = () => {
   return (
     <>
-      <div className="container bg-white mx-auto rounded p-8">
+      <div className="container bg-white mx-auto rounded-4xl p-8">
         <h2 className="text-3xl mb-8">Jenny's UI Interactions Playground</h2>
         <div className="flex gap-4">
-          <DebouncedSearch />
-          <DebouncedSearch />
           <DebouncedSearch />
           <ClickOutsideDropdown />
         </div>
--- src/patterns/ClickOutsideDropdown.tsx
+++ src/patterns/ClickOutsideDropdown.tsx
@@ -1,10 +1,70 @@
+import { useState, useRef, useEffect } from 'react';
 import Card from '../components/Card';
 
 const ClickOutsideDropdown = () => {
-  const content = <div></div>;
+  const [showDropdown, setShowDropdown] = useState(false);
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (!showDropdown) return;
+    document.addEventListener('click', handleClick);
+    document.addEventListener('keydown', handleKeydown);
+
+    return () => {
+      document.removeEventListener('click', handleClick);
+      document.removeEventListener('keydown', handleKeydown);
+    };
+  }, [showDropdown]);
+
+  const handleClick = (e: MouseEvent) => {
+    if (
+      containerRef.current &&
+      !containerRef.current.contains(e.target as Node)
+    ) {
+      setShowDropdown(false);
+    }
+  };
+
+  const handleKeydown = (e: KeyboardEvent) => {
+    if (containerRef.current && e.key === 'Escape') {
+      setShowDropdown(false);
+    }
+  };
+
+  const content = (
+    <div ref={containerRef}>
+      <button
+        className="dropdown-btn bg-sage-green p-1.5 rounded-md text-sm text-white"
+        aria-expanded={showDropdown}
+        onClick={() => setShowDropdown(true)}
+      >
+        Click me!
+      </button>
+      {showDropdown && (
+        <div className="dropdown border border-gray-200 mt-1 p-1">
+          <ul>
+            {[1, 2, 3].map((num: number) => {
+              return (
+                <li
+                  className="hover:bg-gray-100 text-sm"
+                  role="menuitem"
+                  onClick={() => alert(`You have clicked item ${num}!`)}
+                >
+                  item {num}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 
   return (
-    <Card patternName="Click-Outside Dropdown" description="temp description">
+    <Card
+      patternName="Click-Outside Dropdown"
+      description="A dropdown that dismisses on outside clicks and Escape, using refs and document event listeners."
+    >
       {content}
     </Card>
   );
