e08cec4fdcc3049f4cedf8eeeeffbf2788366c83
Jennykuma <jennyle.dev@gmail.com>
2026-05-26 13:03:27 -0600

    Add ARIA to keyboard toggle

--- src/patterns/KeyboardToggle.tsx
+++ src/patterns/KeyboardToggle.tsx
@@ -7,7 +7,7 @@ const KeyboardToggle = () => {
 	const handleKeydown = (e: React.KeyboardEvent<HTMLInputElement>) => {
 		if (e.key === 'Enter') {
 			e.preventDefault();
-			setIsOn(!isOn);
+			setIsOn((value) => !value);
 		}
 	};
 
@@ -15,25 +15,31 @@ const KeyboardToggle = () => {
 		<>
 			<label className="inline-flex cursor-pointer">
 				<input
+					id="keyboard-toggle"
+					aria-checked={isOn}
+					aria-labelledby="keyboard-toggle-label"
 					className="sr-only peer"
 					type="checkbox"
 					role="switch"
 					checked={isOn}
-					onChange={() => setIsOn((value) => !value)}
+					onChange={(e) => setIsOn(e.currentTarget.checked)}
 					onKeyDown={handleKeydown}
 				/>
 				<div
-					className="relative bg-neutral-300 w-8 h-4
-          rounded-full mr-2 mt-0.5 peer
-          peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-pink-100
-          peer-checked:bg-pink-200 peer-checked:after:translate-x-full
-          after:content-[''] after:absolute
-          after:border-0 after:bg-white
-          after:top-[0.5] after:start-[0.5]
-          after:ring-1 after:ring-pink-100
-          after:rounded-full after:h-4 after:w-4 after:transition-all"
+					className="
+						relative bg-neutral-300 w-8 h-4
+						rounded-full mr-2 mt-0.5 peer
+						peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-pink-100
+						peer-checked:bg-pink-200 peer-checked:after:translate-x-full
+						after:content-[''] after:absolute
+						after:border-0 after:bg-white
+						after:top-[0.5] after:start-[0.5]
+						after:ring-1 after:ring-pink-100
+						after:rounded-full after:h-4 after:w-4 after:transition-all"
 				/>
-				<span className="text-sm">Toggle switch</span>
+				<span id="keyboard-toggle-label" className="text-sm">
+					Keyboard toggle
+				</span>
 			</label>
 		</>
 	);
