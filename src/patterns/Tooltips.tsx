6e6bd4be91d75619bc7a930d2a2b496add04bb45
Jennykuma <jennyle.dev@gmail.com>
2026-05-27 22:09:55 -0600

    Add tooltips card

--- src/patterns/Tooltips.tsx
+++ src/patterns/Tooltips.tsx
@@ -1,12 +1,55 @@
+import { useState } from 'react';
 import Card from '../components/Card';
 
 const Tooltips = () => {
+	const [showTooltip, setShowTooltip] = useState(false);
+
+	const handleHideTooltip = () => {
+		setTimeout(() => {
+			setShowTooltip(false);
+		}, 200);
+	};
+
+	const handleKeydown = (e: React.KeyboardEvent<HTMLButtonElement>) => {
+		if (e.key === 'Escape') {
+			setShowTooltip(false);
+		}
+	};
+
+	const content = (
+		<>
+			<button
+				aria-describedby="button-tooltip"
+				className="bg-sage-green p-1.5 rounded-md text-sm text-white cursor-pointer mb-3"
+				onMouseEnter={() => setShowTooltip(true)}
+				onMouseLeave={handleHideTooltip}
+				onFocus={() => setShowTooltip(true)}
+				onBlur={handleHideTooltip}
+				onKeyDown={(e) => handleKeydown(e)}
+			>
+				Hover over me!
+			</button>
+			{showTooltip && (
+				<>
+					<div
+						role="tooltip"
+						id="button-tooltip"
+						className={`w-50 bg-gray-800 text-white text-sm rounded-md p-2 ${showTooltip ? 'block' : 'hidden'}`}
+					>
+						This is a tooltip!
+					</div>
+					<div className="caret-up"></div>
+				</>
+			)}
+		</>
+	);
+
 	return (
 		<Card
 			patternName="Tooltips"
-			description="A tooltip is a small pop-up box that appears when a user hovers over an element, providing additional information about that element. Tooltips are commonly used to provide context or explanations for icons, buttons, or other interactive elements on a webpage."
+			description="A tooltip is a small pop-up box that appears when a user hovers over an element, focuses on it, or touches it"
 		>
-			WIP
+			{content}
 		</Card>
 	);
 };
