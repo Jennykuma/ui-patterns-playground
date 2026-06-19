1844446abf748a6dfb5cdfe2f2727e2b9ec49567
Jenny <jennyle.dev@gmail.com>
2026-06-18 20:50:39 -0600

    Fix asset path with ui-patterns-playground prefix

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import type { InlineConfig } from 'vite';
 
 const config: StorybookConfig = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -10,6 +11,10 @@ const config: StorybookConfig = {
 		'@storybook/addon-onboarding',
 	],
 	framework: '@storybook/react-vite',
+	viteFinal: async (config: InlineConfig) => {
+		config.base = '/ui-patterns-playground/';
+		return config;
+	},
 	docs: {
 		//👇 See the table below for the list of supported options
 		defaultName: 'Documentation',
